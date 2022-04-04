04542642d21253bee6b74768d05d61134c31e67d
amey99 <ameypatankar333@gmail.com>
2022-04-04 13:02:59 +0530

    first commit

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import FormInput from '../form-input/form-input.component';
+import Button, {BUTTON_TYPES_CLASSES} from '../button/button.component';
+
+import {signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword} from '../../utils/firebase/firebase.utils';
+
+
+import './sign-in-form.styles.scss';
+
+const defaultFormField = {
+    email: '',
+    password: ''
+}
+
+const SignInForm = () => {
+    const [formField, setFormField] = useState(defaultFormField);
+    const {email, password} = formField;
+
+    const resetFormField = () => {
+        setFormField(defaultFormField);
+    }
+
+    const signInWithGoogle = async () => {
+        await signInWithGooglePopup();
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try{
+            await signInAuthUserWithEmailAndPassword(email, password);
+            resetFormField();
+        } catch (error) {
+            switch(error.code) {
+                case 'auth/wrong-password': 
+                    alert('Incorrect password for email')
+                    break;
+                case 'auth/user-not-found': 
+                    alert('no user associated with this email')
+                    break;
+                default:
+                    console.log(error)
+            }
+        }
+    }
+
+    const handleChange = (event) => {
+        const {name,value} = event.target;
+        setFormField({...formField, [name]:value})
+    }
+
+    return (
+        <div className="sign-in-container">
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput 
+                    label="email"
+                    type="email"
+                    required
+                    onChange={handleChange}    
+                    name="email"
+                    value={email}
+                />
+                <FormInput 
+                    label="password"
+                    type="password"
+                    required
+                    onChange={handleChange}    
+                    name="password"
+                    value={password}
+                />
+                <div className="buttons-container">
+                    <Button type="submit">
+                        Sign In
+                    </Button>
+                    <Button type="button" onClick={signInWithGoogle} buttonType={BUTTON_TYPES_CLASSES.google}> 
+                        Google Sign In 
+                    </Button>
+                </div>
+            </form>
+        </div> 
+    )
+}
+
+export default SignInForm;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,93 @@
+import {initializeApp} from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword,signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCngVPNUA39-JBm5f1bxP8_gZHi9EAJGYM",
+    authDomain: "crwn-clothing-db-9befc.firebaseapp.com",
+    projectId: "crwn-clothing-db-9befc",
+    storageBucket: "crwn-clothing-db-9befc.appspot.com",
+    messagingSenderId: "497043536756",
+    appId: "1:497043536756:web:34a16d93de17b76ca70a21"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object)=> {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done');
+}
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc,docSnapshot)=> {
+        const {title, items} = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+}
+
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {
+    if(!userAuth) return;
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation
+            })
+        } catch(error) {
+            console.log('error creatign the user', error.messge);
+        }
+    }
+    return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+    return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+    return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
